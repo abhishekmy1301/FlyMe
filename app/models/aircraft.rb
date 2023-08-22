9c53a61b570a4eea3df83a8165c1224edbd774c1
Abhishek Mahendran <abhishekmy1301@gmail.com>
2023-08-22 21:04:30 +0100

    Validations and associations complete

--- app/models/aircraft.rb
+++ app/models/aircraft.rb
@@ -1,3 +1,8 @@
 class Aircraft < ApplicationRecord
   belongs_to :user
+
+  validates :model, presence: true
+  validates :capacity, presence: true, numericality: { greater_than_or_equal_to: 0 }
+  validates :price, presence: true, numericality: { greater_than_or_equal_to: 100 }
+  validates :description, presence: true, length: { minimum: 10 }
 end
--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :aircraft
+
+  validates :departure_time, presence: true
+  validates :takeoff, presence: true, uniqueness: { scope: :landing, message: "Takeoff and Landing can't be the same" }
+  validates :landing, presence: true
 end
