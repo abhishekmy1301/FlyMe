459fc8b0cf66504c29bb450b968a5eabce394854
Abhishek Mahendran <abhishekmy1301@gmail.com>
2023-08-22 14:49:33 +0100

    Created model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_21_151659) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_22_133217) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -23,6 +23,18 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_151659) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_aircrafts_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "aircraft_id", null: false
+    t.datetime "departure_time", precision: nil
+    t.string "takeoff"
+    t.string "landing"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["aircraft_id"], name: "index_bookings_on_aircraft_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -38,4 +50,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_21_151659) do
   end
 
   add_foreign_key "aircrafts", "users"
+  add_foreign_key "bookings", "aircrafts"
+  add_foreign_key "bookings", "users"
 end
